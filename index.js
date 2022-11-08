aed2533edd5ab3924b36801caf78498798f28cf0
Md Al Amin <fpalamin6@gmail.com>
2022-11-08 18:47:35 +0600

    dada get

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,15 +8,34 @@ const port = process.env.PORT || 5000;
 //fooduser11
 //QlPyjswRfRjExvly
 
+// middle wares
+app.use(cors());
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send('food server is running!')
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://fooduser11:QlPyjswRfRjExvly@cluster0.hjwsdej.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const foodsCollection = client.db("foodsService").collection("foods")
+        app.get('/foods', async(req,res)=>{
+            const query = {}
+            const cursor = foodsCollection.find(query)
+            const foods = await cursor.toArray();
+            res.send(foods)
+
+        })
+    }
+    finally{
+
+    }
+}
+run()
 
 app.listen(port,()=>{
     client.connect((err) => {
