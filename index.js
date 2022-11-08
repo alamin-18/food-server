563931745fac83b8c8804cdc64ca46c8350d92f8
Md Al Amin <fpalamin6@gmail.com>
2022-11-08 18:14:35 +0600

    mongodb conected

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+//fooduser11
+//QlPyjswRfRjExvly
+
 app.get('/',(req,res)=>{
     res.send('food server is running!')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://fooduser11:QlPyjswRfRjExvly@cluster0.hjwsdej.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.listen(port,()=>{
+    client.connect((err) => {
+        // Important to check for errors
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Connected to MongoDB");
+        }
+      });
+      
     console.log("Food server running on",port);
 })
